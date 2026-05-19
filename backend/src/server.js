0d6e369ef6a35e1364d450eee460b20499530660
Krish <krishpalat9@gmail.com>
2026-05-19 15:22:52 +0530

    server.js changes

--- backend/src/server.js
+++ backend/src/server.js
@@ -24,12 +24,6 @@ app.get("/", (req, res) => {
 app.use("/api/auth", authRoutes);
 app.use("/api/notes", noteRoutes);
 
-const PORT = process.env.PORT || 5000;
 
-if (process.env.NODE_ENV !== "production") {
-  app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-  });
-}
 
 export default app;
